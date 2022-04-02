3ec2060915427fe51eb495a5fc1daf3dffd83ec8
Muhammed Ali Açış <ykpacs@gmail.com>
2022-04-02 03:43:21 +0300

    Background Changed.

--- Pinpong Game.py
+++ Pinpong Game.py
@@ -3,62 +3,67 @@ import turtle
 pencere = turtle.Screen()
 pencere.title("PinPong")
 pencere.bgcolor("lightgray")
+pencere.bgpic('halisaha.gif')
 pencere.setup(width=800, height=600)
 pencere.tracer(0)
 
 raket_a = turtle.Turtle()
 raket_a.speed(0)
 raket_a.shape('square')
-raket_a.color('red')
+raket_a.color('white')
 raket_a.penup()
 raket_a.goto(-350, 0)
-raket_a.shapesize(5, 1)
+raket_a.shapesize(6, 1)
 
 raket_b = turtle.Turtle()
 raket_b.speed(0)
 raket_b.shape('square')
-raket_b.color('blue')
+raket_b.color('white')
 raket_b.penup()
 raket_b.goto(350, 0)
-raket_b.shapesize(5, 1)
+raket_b.shapesize(6, 1)
 
 ball = turtle.Turtle()
 ball.speed(0)
 ball.shape('circle')
-ball.color('black')
+ball.color('white')
 ball.penup()
-ball.dx = 0.15
-ball.dy = 0.15
+ball.dx = 0.19
+ball.dy = 0.19
 
 yazi = turtle.Turtle()
 yazi.speed(0)
-yazi.color('black')
+yazi.color('white')
 yazi.penup()
-yazi.goto(0, 250)
-yazi.write("OyuncuA:0   OyuncuB:0", align='center', font=('Courier', 24, "bold"))
+yazi.goto(0, 270)
+yazi.write("Player A = 0   Player B = 0", align='center', font=('Courier', 17, "bold"))
 yazi.hideturtle()
 puan_a = 0
 puan_b = 0
 
 def raket_a_up():
     y = raket_a.ycor()
-    y = y + 25
-    raket_a.sety(y)
+    if y < 250:
+        y = y + 25
+        raket_a.sety(y)
 
 def raket_a_down():
     y = raket_a.ycor()
-    y = y - 25
-    raket_a.sety(y)
+    if y > -250:
+        y = y - 25
+        raket_a.sety(y)
 
 def raket_b_up():
     y = raket_b.ycor()
-    y = y + 25
-    raket_b.sety(y)
+    if y < 250:
+        y = y + 25
+        raket_b.sety(y)
 
 def raket_b_down():
     y = raket_b.ycor()
-    y = y - 25
-    raket_b.sety(y)
+    if y > -250:
+        y = y - 25
+        raket_b.sety(y)
 
 pencere.listen()
 pencere.onkeypress(raket_a_up, 'w')
@@ -79,19 +84,19 @@ while True:
         ball.dx = ball.dx * -1
         puan_a = puan_a + 1
         yazi.clear()
-        yazi.write("OyuncuA:{}  OyuncuB:{}".format(puan_a, puan_b), align='center', font=('Courier', 24, "bold"))
+        yazi.write("Player A = {}  Player B = {}".format(puan_a, puan_b), align='center', font=('Courier', 17, "bold"))
 
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx = ball.dx * -1
         puan_b = puan_b + 1
         yazi.clear()
-        yazi.write("OyuncuA:{}  OyuncuB:{}".format(puan_a, puan_b), align='center', font=('Courier', 24, "bold"))
+        yazi.write("Player A = {}  Player B = {}".format(puan_a, puan_b), align='center', font=('Courier', 17, "bold"))
 
-    if (ball.xcor() > 340 and ball.xcor() > 350) and (ball.ycor() < raket_b.ycor() + 60 and ball.ycor() > raket_b.ycor() - 60):
-        ball.setx(340)
-        ball.dx = ball.dx * -1
+    if (ball.xcor() > 340 and ball.xcor() <= 350) and (ball.ycor() < raket_b.ycor() + 60 and ball.ycor() > raket_b.ycor() - 60):
+        ball.setx(330)
+        ball.dx = ball.dx * - 1
 
-    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < raket_a.ycor() + 60 and ball.ycor() > raket_a.ycor() - 60):
-        ball.setx(-340)
-        ball.dx = ball.dx * -1
+    if (ball.xcor() < -340 and ball.xcor() >= -350) and (ball.ycor() < raket_a.ycor() + 60 and ball.ycor() > raket_a.ycor() - 60):
+        ball.setx(-330)
+        ball.dx = ball.dx * - 1
